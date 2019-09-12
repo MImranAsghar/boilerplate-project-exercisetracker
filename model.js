a7503c959a4fd21586b6fd24dec1f31ad59c753c
MImranAsghar <imranasghar96@hotmail.com>
2019-09-11 22:57:03 -0400

    Update model.js

--- model.js
+++ model.js
@@ -1,1 +1,8 @@
+const mySchema = require('./schema');
+const mongoose = require('mongoose');
 
+let theModel = mongoose.model("theModel", schemas.exerciseSchema);
+
+module.exports = {
+  theModel: theModel
+}
