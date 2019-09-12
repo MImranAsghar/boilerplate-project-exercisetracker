7d3109c518d9e4f31135f80651e94bb37515c58b
MImranAsghar <imranasghar96@hotmail.com>
2019-09-11 22:50:58 -0400

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose");
 
+const exerciseSchema = new mongoose.Schema({
+  user: String,
+  exercise: {
+    desc: String,
+    timeDuration: Number,
+    date: {type: Date, default: Date.now}
+  }
+});
+
+module.exports = {
+  exerciseSchema: exerciseSchema
+};
